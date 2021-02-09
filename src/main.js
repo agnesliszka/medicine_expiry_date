b6523a2729f7670fd58c21e4be04949afdd7e481
Agnieszka <agnesliszk@gmail.com>
2021-02-09 20:31:08 +0100

    Change date-picker's month names to english

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import ElementUI from "element-ui";
 import App from "./App.vue";
+import locale from "element-ui/lib/locale/lang/en";
 import { store } from "./store/store.js";
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 window.axios = require("axios");
 
 new Vue({
